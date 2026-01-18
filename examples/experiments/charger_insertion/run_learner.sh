638ecbd6953626d156d4bcdd48ab8c6bb960804a
shenzhaolong <shenzhaolong1330@mail.com>
2026-01-18 15:35:47 +0800

    run

--- examples/experiments/charger_insertion/run_learner.sh
+++ examples/experiments/charger_insertion/run_learner.sh
@@ -1,7 +1,7 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python ../../train_rlpd.py "$@" \
-    --exp_name=ram_insertion \
+    --exp_name=charger_insertion \
     --checkpoint_path=first_run \
-    --demo_path=... \
+    --demo_path=demo_data/charger_insertion_20_demos_2026-01-18_14-35-20.pkl \
     --learner \
